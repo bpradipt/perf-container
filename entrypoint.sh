7c9bed226c37012af6b75a7c3687b1f22343a0b7
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2018-06-05 07:10:54 +0000

    Update entrypoint with loop and file output support for perf exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+TIMEOUT=${1:-60}
+COUNT=${2:-1}
+
 declare -a PIDS
 PIDS=($(ps -A -o pid=))
 
@@ -10,9 +13,16 @@ echo "Monitored PIDs: " "${PIDS[*]}"
 PERF_PIDS=`(IFS=$','; echo "${PIDS[*]}")`
 
 function start_perf() {
-   timeout -t 60  /perf stat -a -p $PERF_PIDS -x ',' -I 1000
+   for (( i=1; i<=${COUNT}; i++ ))
+   do
+     timeout -t ${TIMEOUT}  /perf stat -a -p $PERF_PIDS -x ',' -I 1000  -A -o /out/perf-stat-output --append
+   done
 }
 
+
+#Trigger perf collection if /tmp/startperf is present
 while read i; do if [ "$i" = startperf ]; then start_perf; break; fi; done \
 < <(inotifywait  -e create,open,modify --format '%f' --monitor /tmp)
 
+#Long sleep :-)
+sleep 1000000000d
