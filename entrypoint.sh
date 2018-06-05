f3e795cd8624e7f8fc4542ff92bbe01a65e7e0c1
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2018-06-05 17:15:30 +0000

    Add support for perf record

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-TIMEOUT=${1:-60}
-COUNT=${2:-1}
+#entrypoint.sh <stat|record|probe> <trigger[1|0]> <max-run-time[X]> <repeat-count[N]> <extra-perf-args>
+PERF_TYPE=${1:-record}
+TRIGGER=${2:-0}
+TIMEOUT=${3:-60}
+COUNT=${4:-1}
+EXTRA_PERF_ARGS=$5
 OUT_SUFFIX=${HOSTNAME:+-$HOSTNAME}
+
 declare -a PIDS
 PIDS=($(ps -A -o pid=))
 
@@ -12,17 +17,46 @@ echo "Monitored PIDs: " "${PIDS[*]}"
 #Convert array to comma separated list
 PERF_PIDS=`(IFS=$','; echo "${PIDS[*]}")`
 
-function start_perf() {
+function start_perf_stat() {
    for (( i=1; i<=${COUNT}; i++ ))
    do
-     timeout -t ${TIMEOUT}  /perf stat -a -p $PERF_PIDS -x ',' -I 1000  -A -o /out/perf-stat-output${OUT_SUFFIX} --append
+     echo "timeout -t ${TIMEOUT}  /perf stat -a -p $PERF_PIDS -x ',' -I 1000  -A -o /out/perf-stat-output${OUT_SUFFIX} --append ${EXTRA_PERF_ARGS}"
+     timeout -t ${TIMEOUT}  /perf stat -a -p $PERF_PIDS -x ',' -I 1000  -A -o /out/perf-stat-output${OUT_SUFFIX} --append ${EXTRA_PERF_ARGS}
+   done
+}
+
+function start_perf_record() {
+   for (( i=1; i<=${COUNT}; i++ ))
+   do
+     echo "timeout -t ${TIMEOUT}  /perf record -a -g -p $PERF_PIDS -o /out/perf-record-output${OUT_SUFFIX} ${EXTRA_PERF_ARGS}"
+     timeout -t ${TIMEOUT}  /perf record -a -p $PERF_PIDS -o /out/perf-record-output${OUT_SUFFIX} ${EXTRA_PERF_ARGS}
    done
 }
 
 
-#Trigger perf collection if /tmp/startperf is present
-while read i; do if [ "$i" = startperf ]; then start_perf; break; fi; done \
-< <(inotifywait  -e create,open,modify --format '%f' --monitor /tmp)
+case ${PERF_TYPE} in 
+  stat )
+     if [ "${TRIGGER}" == "1" ]
+     then
+         #Trigger perf collection if /tmp/startperf is present
+         while read i; do if [ "$i" = startperf ]; then start_perf_stat; break; fi; done \
+         < <(inotifywait  -e create,open,modify --format '%f' --monitor /tmp)
+     else
+         start_perf_stat
+     fi
+     ;;
+  record ) 
+     if [ "${TRIGGER}" == "1" ]
+     then
+         #Trigger perf collection if /tmp/startperf is present
+         while read i; do if [ "$i" = startperf ]; then start_perf_record; break; fi; done \
+         < <(inotifywait  -e create,open,modify --format '%f' --monitor /tmp)
+     else
+         start_perf_record
+     fi
+     ;;
+   
+esac
 
 #Long sleep :-)
 sleep 1000000000d
